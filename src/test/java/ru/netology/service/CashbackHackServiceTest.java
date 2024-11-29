63c609b6afc635b5b0c452c46ade3221e5f3d430
ynappva@redmachinegames.com <cC255NQxG>
2024-11-29 21:54:43 +0300

    testng added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,46 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+    public void tellsHowMuchToPayIfAmountLessThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+
+        }
+
+        @Test
+    public void tellsNothingIfAmountIsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+
+        }
+
+        @Test
+    public void tellsHowMuchToPayIfAmountMoreThanBoundary(){
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1500;
+
+            int actual = service.remain(amount);
+            int expected = 500;
+
+            Assert.assertEquals(actual, expected);
+
+        }
     }
-}
